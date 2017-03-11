571426cddf638952da476b6d35629708e0d980e2
DanielSrb <srb.dan@gmail.com>
2017-03-11 01:23:45 +0100

    sampler out

--- plugin/sampler/example/many-samples.cpp
+++
@@ -1,8 +0,0 @@
-
-// sample(sample-1)
-void this_is_sample_1() { }
-// end-sample
-
-// sample(sample-2)
-void this_is_sample_2() { }
-// end-sample
--- plugin/sampler/example/sample.c
+++
@@ -1,8 +0,0 @@
-
-/* sample(main) */
-#include <stdio.h>
-
-int main(int argc, char ** argv) {
-    printf("Hello world!\n");
-}
-/* end-sample */
